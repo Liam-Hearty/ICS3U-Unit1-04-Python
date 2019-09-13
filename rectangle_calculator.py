153f276a70ae6029e7857dc0f8c884c582eb2a8b
Ubuntu <ubuntu@ip-172-31-17-81.ec2.internal>
2019-09-13 20:16:07 +0000

    added program

---
+++ rectangle_calculator.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+# Created by: Liam Hearty
+# Created on: September 2019
+# This program will calculate the Area and Perimeter of a 3mm by 5mm rectangle
+
+
+def main():
+    print("If a rectangle has the dimensions: 3mm x 5mm")
+    print("")
+    print("A=lw")
+    print("3x5={}".format(3*5))
+    print("")
+    print("P=2(l+w)")
+    print("2(3+5)=2x{}".format(3+5))
+    print("2x8={}".format(2*8))
+
+
+if __name__ == "__main__":
+    main()
+    
